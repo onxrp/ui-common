6fdf178ca562f69d413e7ced7851b72713b53e57
ivdznk <dzizenko.i@gmail.com>
2022-02-23 00:14:32 +0200

    Move to typescripts

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import {
+	Box,
+	AppBar,
+	Toolbar,
+	useTheme,
+	Container,
+	Link as MuiLink,
+	Button,
+} from '@mui/material'
+import React from 'react'
+
+const Header: React.FC<any> = () => {
+	const theme = useTheme()
+	return (
+		<>
+			<Box sx={{ flexGrow: 1 }} borderBottom={`4px solid #a100b9`}>
+				<AppBar position="static"
+					// @ts-ignore
+					color="white">
+					<Container>
+						<Toolbar disableGutters={true}>
+							<Box sx={{ flexGrow: 1 }} textAlign="left">
+								<MuiLink href="https://onxrp.com/">
+									<img
+										src="/assets/images/logo.png"
+										style={{ maxWidth: '150px' }}
+										alt="onXRP"
+									/>
+								</MuiLink>
+							</Box>
+							<Box>
+								<MuiLink href="https://onxrp.com/">
+									<Button color="primary" variant="contained">
+										Back to Home
+									</Button>
+								</MuiLink>
+							</Box>
+						</Toolbar>
+					</Container>
+				</AppBar>
+			</Box>
+		</>
+	)
+}
+
+export default Header;
