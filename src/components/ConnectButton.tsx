ad14b138032af42b421dc2e1ffdd28e241970abe
ivdznk <dzizenko.i@gmail.com>
2022-03-06 21:57:40 +0200

    Added connect wallet button

---
+++ src/components/ConnectButton.tsx
@@ -0,0 +1,204 @@
+import { useContext, useEffect, useState, useRef, useCallback } from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  Dialog,
+  DialogContent,
+  CircularProgress,
+  useTheme,
+} from "@mui/material";
+import axios from "axios";
+// @ts-ignore
+import { ReactSession } from "react-client-session";
+import _ from "lodash";
+import { formatNumber } from "../Utils";
+import { GlobalContext } from "../contexts/GlobalState";
+
+export default function ConnectButton() {
+  const theme = useTheme();
+  const { state, loadAccount, signOutAccount } = useContext(GlobalContext);
+  const activeInterval = useRef<any>();
+  const [uuid, setUuid] = useState(null);
+  const [signObj, setSignObj] = useState(null);
+  const [qrCode, setQrcode] = useState(null);
+  const [isSigning, setIsSigning] = useState(false);
+  const [signInLink, setSignInLink] = useState(null);
+
+  useEffect(() => {
+    if (uuid) {
+      activeInterval.current = setInterval(() => {
+        axios
+          .get(`/api/trade/get-payload/${uuid}`)
+          .then((res: any) => {
+            const account = res?.data?.response?.account;
+            if (account) {
+              ReactSession.set("xpunk-account", account);
+              ReactSession.set("xpunk-xummObj", res?.data);
+              clearInterval(activeInterval.current);
+              loadAccount();
+              setUuid(null);
+              setSignObj(null);
+              setQrcode(null);
+              setIsSigning(false);
+            }
+          })
+          .catch((err: any) => {
+            console.log(err);
+          });
+      }, 3000);
+    }
+  }, [uuid]);
+
+  const _handleConnectAccount = () => {
+    axios
+      .post(`/api/account/connect-wallet`)
+      .then((res: any) => {
+        setUuid(res.data.uuid);
+        setSignObj(res.data);
+        setSignInLink(res.data.next.always);
+        setQrcode(res.data.refs.qr_png);
+        setIsSigning(true);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+
+  const _handleConnectAccountDebounce = useCallback(
+    _.debounce(_handleConnectAccount, 500),
+    []
+  );
+
+  const _cancelTransaction = () => {
+    axios
+      .get(`/api/trade/cancel-payload/${uuid}`)
+      .then((res: any) => {
+        setUuid(null);
+        setSignObj(null);
+        setQrcode(null);
+        clearInterval(activeInterval.current);
+        setIsSigning(false);
+      })
+      .catch((err: any) => {
+        console.log("err", err);
+      });
+  };
+
+  const _cancelTransactionDebounce = useCallback(
+    _.debounce(_cancelTransaction, 500),
+    [uuid, activeInterval]
+  );
+
+  return (
+    <>
+      {isSigning ? (
+        <Dialog
+          open={isSigning}
+          keepMounted
+          // @ts-ignore
+          PaperComponent="div"
+        >
+          <DialogContent
+            sx={{
+              backgroundColor: "#040f30",
+              borderRadius: "15px",
+              padding: { md: 4, xs: 2 },
+              textAlign: "center",
+            }}
+          >
+            <Typography
+              color={(theme.palette as any).white.main}
+              fontSize="1.2rem"
+              marginBottom={2}
+            >
+              SIGN IN WITH XUMM
+            </Typography>
+            {signInLink ? (
+              <a
+                href={signInLink}
+                rel="noreferrer"
+                // @ts-ignore
+                alt="Open in Xumm"
+                style={{
+                  color: theme.palette.primary.main,
+                  textDecoration: "none",
+                  display: "block",
+                  marginBottom: "1.2rem",
+                }}
+              >
+                <Button
+                  fullWidth
+                  variant="contained"
+                  className="gradient-button"
+                  sx={{
+                    p: 1.5,
+                    borderRadius: "15px",
+                    display: { md: "none", xs: "block" },
+                  }}
+                >
+                  Click Here to Open in Xumm
+                </Button>
+              </a>
+            ) : null}
+            {qrCode ? (
+              <img
+                src={qrCode}
+                alt="Scan QR Code"
+                style={{ maxWidth: "250px", width: "100%" }}
+              />
+            ) : (
+              <CircularProgress />
+            )}
+            <Button
+              // @ts-ignore
+              variant="secondary"
+              onClick={_cancelTransactionDebounce}
+              fullWidth
+              sx={{
+                mt: "30px",
+                p: 1,
+                borderRadius: "15px",
+                color: "white.main",
+              }}
+            >
+              Cancel
+            </Button>
+          </DialogContent>
+        </Dialog>
+      ) : null}
+      {!state.account ? (
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={_handleConnectAccountDebounce}
+        >
+          Connect Wallet
+        </Button>
+      ) : (
+        <>
+          <Box display={{ md: "flex", xs: "none" }}>
+            {state.altCoin ? (
+              <>
+                <Typography>
+                  {formatNumber(state.altBalance, 4)} {state.altCoin.ticker}
+                </Typography>
+                <Typography sx={{ mx: "20px" }}>|</Typography>
+              </>
+            ) : null}
+            <Typography>{formatNumber(state.xrpBalance, 4)} XRP</Typography>
+          </Box>
+          <Box display={{ md: "table" }} ml={2}>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={signOutAccount}
+            >
+              Disconnect
+            </Button>
+          </Box>
+        </>
+      )}
+    </>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,66 @@
-import React from 'react'
+import React from "react";
 import {
-	Box,
-	AppBar,
-	Toolbar,
-	useTheme,
-	Container,
-	Link as MuiLink,
-	Button,
-} from '@mui/material'
-import IconButton from '@mui/material/IconButton';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+  Box,
+  AppBar,
+  Toolbar,
+  useTheme,
+  Container,
+  Link as MuiLink,
+  Button,
+} from "@mui/material";
 
 import Logo from "./Logo";
+import ConnectButton from "./ConnectButton";
 
 interface HeaderProps {
-	onChangeColorTheme: () => void
+  onChangeColorTheme: () => void;
+  hasWallet: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({ onChangeColorTheme }) => {
-	const theme = useTheme()
-	return (
-		<>
-			<Box sx={{ flexGrow: 1 }} borderBottom={`4px solid #a100b9`}>
-				<AppBar position="static"
-					// @ts-ignore
-					color="white">
-					<Container>
-						<Toolbar disableGutters={true}>
-							<Box sx={{ flexGrow: 1 }} textAlign="left">
-								<Logo />
-							</Box>
-							<Box>
-								<Button
-									// @ts-ignore
-									color="button"
-									variant="contained"
-									sx={{ marginRight: '20px' }}
-									onClick={onChangeColorTheme}
-								>
-									{theme.palette.mode === 'dark' ? "Light mode" : "Dark mode"}
-								</Button>
-							</Box>
-							<Box>
-								<MuiLink href="https://onxrp.com/">
-									<Button color="primary" variant="contained">
-										Back to Home
-									</Button>
-								</MuiLink>
-							</Box>
-						</Toolbar>
-					</Container>
-				</AppBar>
-			</Box>
-		</>
-	)
-}
+const Header: React.FC<HeaderProps> = ({ onChangeColorTheme, hasWallet = false }) => {
+  const theme = useTheme();
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} borderBottom={`4px solid #a100b9`}>
+        <AppBar
+          position="static"
+          // @ts-ignore
+          color="white"
+        >
+          <Container>
+            <Toolbar disableGutters={true}>
+              <Box sx={{ flexGrow: 1 }} textAlign="left">
+                <Logo />
+              </Box>
+              <Box>
+                <Button
+                  // @ts-ignore
+                  color="button"
+                  variant="contained"
+                  sx={{ marginRight: "20px" }}
+                  onClick={onChangeColorTheme}
+                >
+                  {theme.palette.mode === "dark" ? "Light mode" : "Dark mode"}
+                </Button>
+              </Box>
+              <Box>
+                <MuiLink href="https://onxrp.com/">
+                  <Button color="primary" variant="contained">
+                    Back to Home
+                  </Button>
+                </MuiLink>
+              </Box>
+			  {hasWallet &&
+				<Box>
+					<ConnectButton />
+				</Box>
+			  }
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
 
 export default Header;
