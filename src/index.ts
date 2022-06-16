ab88db11c465ac70b26c13ad9c18c34af4b57bfc
ivdznk <dzizenko.i@gmail.com>
2022-06-16 20:17:14 +0300

    added ConnectButton to export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { default as Footer } from './components/Footer';
-export { default as Header } from './components/Header';
-export { default as Logo } from './components/Logo';
-export { formatNumber, average } from './utils';
+export { default as Footer } from "./components/Footer";
+export { default as Header } from "./components/Header";
+export { default as Logo } from "./components/Logo";
+export { default as ConnectButton } from "./components/ConnectButton";
+export { formatNumber, average } from "./utils";
