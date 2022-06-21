398db87fe97eb3bfed66bd685bba7e2dc076c162
ivdznk <dzizenko.i@gmail.com>
2022-06-21 17:00:40 +0300

    Added close icon

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as Header } from "./components/Header";
 export { default as Logo } from "./components/Logo";
 export { default as ConnectButton } from "./components/ConnectButton";
 export { default as ProjectsButtons } from "./components/ProjectsButtons";
+export { default as CloseButton } from "./components/ProjectsButtons";
 export { formatNumber, average } from "./utils";
